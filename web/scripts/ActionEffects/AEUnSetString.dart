982a2a695fb933f190a223aa64c8a6b984f38310
jadedResearcher <gtg287y@gmail.com>
2020-04-27 22:42:26 -0400

    core refactoring done but things calling these classes are still fucked

--- web/scripts/ActionEffects/AEUnSetString.dart
+++ web/scripts/ActionEffects/AEUnSetString.dart
@@ -2,11 +2,12 @@ import '../Entity.dart';
 import 'ActionEffect.dart';
 
 class AEUnSetString extends ActionEffect {
-    AEUnSetString(String importantString, num importantNum) : super(importantString, importantNum);
+    static const String KEY = "memoryKey";
+    AEUnSetString(String key) : super({KEY:key},{});
 
   @override
   void effectEntities(Entity effector, List<Entity> entities) {
-    entities.forEach((Entity e) => e.removeStringMemoryKey(importantString));
+    entities.forEach((Entity e) => e.removeStringMemoryKey(importantWords[KEY]));
   }
 
 }
