0cd95bdf97a7109cb02f22cc8ea913686720f3df
jadedResearcher <gtg287y@gmail.com>
2020-04-27 22:20:06 -0400

    refactoring for serialization

--- web/scripts/ActionEffects/AESetDollStringFromYourMemory.dart
+++ web/scripts/ActionEffects/AESetDollStringFromYourMemory.dart
@@ -2,12 +2,14 @@ import '../Entity.dart';
 import 'ActionEffect.dart';
 
 class AESetDollStringFromYourMemory extends ActionEffect {
-    AESetDollStringFromYourMemory(String importantString, num importantNum) : super(importantString, importantNum);
+    static const String SHAREDKEY = "memoryKey";
+
+    AESetDollStringFromYourMemory(String sharedKey) : super({SHAREDKEY:sharedKey}, {});
 
   @override
   void effectEntities(Entity effector, List<Entity> entities) {
-      print("I'm going to set $entities dollstrings to ${entities.first.getStringMemory(importantString)}");
-    entities.forEach((Entity e) => e.setNewDoll(e.getStringMemory(importantString)));
+      print("I'm going to set $entities dollstrings to ${entities.first.getStringMemory(importantWords[SHAREDKEY])}");
+    entities.forEach((Entity e) => e.setNewDoll(e.getStringMemory(importantWords[SHAREDKEY])));
   }
 
 }
