b4ae40025c2e7a3875af7254f158537385da78d8
jadedResearcher <gtg287y@gmail.com>
2020-04-27 22:25:26 -0400

    refactoring for serialization

--- web/scripts/ActionEffects/AESetNum.dart
+++ web/scripts/ActionEffects/AESetNum.dart
@@ -2,11 +2,13 @@ import '../Entity.dart';
 import 'ActionEffect.dart';
 
 class AESetNum extends ActionEffect {
-  AESetNum(String importantString, num importantNum) : super(importantString, importantNum);
+  static const String KEY = "memoryKey";
+  static const String NUM = "num";
+  AESetNum(key,num) : super({KEY:key}, {NUM:num});
 
   @override
   void effectEntities(Entity effector, List<Entity> entities) {
-    entities.forEach((Entity e) => e.setNumMemory(importantString, importantNum));
+    entities.forEach((Entity e) => e.setNumMemory(importantWords[KEY], importantNumbers[NUM]));
   }
 
 }
