65d4a1c4d042966d9972393b2d9809158c88beb9
jadedResearcher <gtg287y@gmail.com>
2020-05-03 22:13:14 -0400

    in theory datastrings are working

---
+++ web/scripts/DataStringHelper.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'dart:html';
+
+import 'package:CommonLib/Compression.dart';
+
+abstract class DataStringHelper {
+    static String labelPattern = ":___ ";
+
+    static String serializationToDataString(String label, Map<String,dynamic> serialization) {
+        //first turn serialization into json
+        String json = jsonEncode(serialization);
+        try {
+            return "${label.replaceAll(',','')}$labelPattern${LZString.compressToEncodedURIComponent(json)}";
+        }catch(e) {
+            print(e);
+            window.alert("Error Saving Data. Are there any special characters in there? $json} $e");
+        }
+    }
+
+    static Map<String,dynamic> serializationFromDataString(String dataString) {
+        List<String> parts = dataString.split("$labelPattern");
+        //print("parts are $parts");
+        if(parts.length > 1) {
+            dataString = parts[1];
+        }
+        String rawJSON = LZString.decompressFromEncodedURIComponent(dataString);
+        return jsonDecode(rawJSON);
+    }
+}
