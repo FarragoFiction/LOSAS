629a0087315c577b403d0d3278af1ba2e45298cf
jadedResearcher <gtg287y@gmail.com>
2020-05-12 22:30:54 -0400

    tiny navbar

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 
+import 'package:CommonLib/NavBar.dart';
+
 import 'scripts/FormHelpers/SceneFormHelper.dart';
 import 'scripts/Game.dart';
 import 'scripts/Scenario.dart';
@@ -8,8 +10,11 @@ import 'package:DollLibCorrect/DollRenderer.dart';
 
 void main() async {
   await Doll.loadFileData();
+  handleVoid();
 
   Element output = querySelector("#output");
+  debugLinks(output);
+
   //TODO modes for all builders, testing and then actually playing
   if(Uri.base.queryParameters['mode'] == "sceneBuilder") {
     await SceneFormHelper.makeSceneBuilder(output);
@@ -17,5 +22,14 @@ void main() async {
     Game game = new Game(Scenario.testScenario());
     UnitTests.runTests(output);
   }
+}
+
+void debugLinks(Element parent) {
+  AnchorElement seerOfVoid = new AnchorElement(href: "${window.location.href}?seerOfVoid=true")..text = "| seerOfVoid |";
+  AnchorElement sceneBuilder = new AnchorElement(href: "${window.location.href}?mode=sceneBuilder")..text = "| sceneBuilder |";
+  AnchorElement runTests = new AnchorElement(href: "${window.location.href}")..text = "| runTests |";
+  parent.append(seerOfVoid);
+  parent.append(sceneBuilder);
+  parent.append(runTests);
 }
 
