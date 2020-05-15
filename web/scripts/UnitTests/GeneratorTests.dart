09257389f7fb845e26cb6ca9de88909a79eca496
jadedResearcher <gtg287y@gmail.com>
2020-05-14 23:09:53 -0400

    generators can be serialized

--- web/scripts/UnitTests/GeneratorTests.dart
+++ web/scripts/UnitTests/GeneratorTests.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 import 'package:CommonLib/Random.dart';
 
+import '../DataStringHelper.dart';
 import "UnitTests.dart";
 abstract class GeneratorTests {
     static void run(Element element) {
@@ -18,6 +19,24 @@ abstract class GeneratorTests {
         num generatedDouble = doubleGenerator.generateValue(new Random(13));
         UnitTests.processTest("Generator should pick number between -1.01 and 1.01, picked $generatedDouble", true, generatedDouble >=-1.01 && generatedDouble <=1.01, element);
 
+        testSerialization(element);
+
+    }
+
+    static void testSerialization(element) {
+        //do one specific example.
+        final Generator numGen = new NumGenerator("secretNumber",13,133);
+        final Generator stringGen = new StringGenerator("secretWord",<String>["hello","world"]);
+
+        Map<String, dynamic> numSerialization = numGen.getSerialization();
+        Map<String, dynamic> stringSerialization = stringGen.getSerialization();
+
+        final Generator recoveredNum = Generator.fromSerialization(numSerialization);
+        final Generator recoveredString = Generator.fromSerialization(stringSerialization);
+
+        UnitTests.processTest("String Generator can be serialized to and from datastring",DataStringHelper.serializationToDataString("TestEffect",stringGen.getSerialization()), DataStringHelper.serializationToDataString("TestEffect",recoveredString.getSerialization()), element);
+
+        UnitTests.processTest("Num Generator can be serialized to and from datastring",DataStringHelper.serializationToDataString("TestEffect",numGen.getSerialization()), DataStringHelper.serializationToDataString("TestEffect",recoveredNum.getSerialization()), element);
 
     }
 }
