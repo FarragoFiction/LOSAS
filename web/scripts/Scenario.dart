5e9aa82de9d33073d556c5a9b40a86809d39cef2
jadedResearcher <gtg287y@gmail.com>
2020-05-25 18:07:34 -0400

    desturctively ripped out the 'run a scenario' code from scenario and put it into game for more cohesive serialization. red squiggles ahoy

--- web/scripts/Scenario.dart
+++ web/scripts/Scenario.dart
@@ -41,26 +41,17 @@ class Scenario extends DataObject {
     Game game;
     //TODO let people sign their work
     String author = "???";
-    //seriously if NOTHING HAPPENS for 13 ticks in a row, lets just call it
-    int numberTriesForScene = 0;
-    int maxNumberTriesForScene = 13;
+
     String description;
 
-    bool theEnd = false;
+
 
 
 
     Random rand;
 
-    List<Entity> _entities = new List<Entity>();
-    List<Entity> get entitiesReadOnly  => _entities;
 
-    //the person in the spotlight is on screen right now
-    Entity spotLightEntity;
-    //if not entities are active on spawn, nothing can happen. I advise having at least an invisible entity, like "Skaia".
-    List<Entity> get activeEntitiesReadOnly => _entities.where((Entity entity) =>entity.isActive).toList();
-
-    //if ANY of these trigger, then its time to stop ticking
+    //optional intro
     List<Scene> frameScenes = new List<Scene>();
 
     //if ANY of these trigger, then its time to stop ticking
@@ -73,126 +64,15 @@ class Scenario extends DataObject {
     }
 
 
-    void curtainsUp(Element parent) {
-        print("curtains are going up");
-        game = new Game(this);
-        print("made a new game");
-        game.setup(parent);
-        initializeEntities();
-        print("setup the game");
-        lookForNextScene();
-        print("looked for a scene");
-    }
-
-    void initializeEntities() {
-        _entities.forEach((Entity e) => e.processPrepacks(rand));
-    }
-
-    void addEntity(Entity entity) {
-        _entities.add(entity);
-        entity.scenario = this;
-    }
 
 
 
-    void debugScenario() {
-        entitiesReadOnly.forEach((Entity e)=> print(e.debugString()));
-    }
-
-    //if no scenes trigger, no introduction
-    Scene getIntroduction() {
-        for(final Scene scene in frameScenes) {
-            final bool active = scene.checkIfActivated(activeEntitiesReadOnly);
-            if(active){
-                return  scene;
-            }
-        }
-        return null;
-    }
-
-
-    //unlike sburbsim this doesn't just tick infinitely. instead it renders a button for next.
-    //if you try to page to the right and the scene doesn't exist and we aren't ended, do this
-    //first you check all your entities
-    //then you check your stop scenes
-    //then you repeat
-    void lookForNextScene() {
-        if(theEnd) return;
-        game.readyForNextScene = false;
-        if(game.sceneElements.length == 0){
-            Scene introduction = getIntroduction();
-            if(introduction != null) {
-                game.showScene(introduction);
-                return;
-            }
-        }
-        //could be some amount of randomness baked in
-        if(numberTriesForScene > maxNumberTriesForScene) {
-            window.alert("something has gone wrong, went $numberTriesForScene loops without anything happening");
-        }
-        Scene spotlightScene;
-        List<Entity> entitiesToCheck = null;
-        if(spotLightEntity != null) {
-            entitiesToCheck = entitiesReadOnly.sublist(entitiesReadOnly.indexOf(spotLightEntity)+1);
-        }else {
-            //every time we get to the start of entities, we shuffle so its not so samey
-            _entities.shuffle(rand);
-            entitiesToCheck = entitiesReadOnly;
-        }
-        spotlightScene = checkEntitiesForScene(entitiesToCheck);
-        if(spotlightScene != null) {
-            game.showScene(spotlightScene);
-        }else {
-            print("Time to check stop scenes");
-            spotlightScene = checkStopScenes();
-            if(spotlightScene == null) {
-                numberTriesForScene ++;
-                spotLightEntity = null;
-                lookForNextScene();
-            }else {
-                theEnd = true;
-                game.showScene(spotlightScene);
-            }
-        }
-    }
-
-    Scene checkStopScenes() {
-      for(final Scene scene in stopScenes) {
-          scene.scenario ??=this;
-          print("checking stop scene $scene with activeEntities $activeEntitiesReadOnly, btw the scene has these filters ${scene.targetFilters}");
-          final bool active = scene.checkIfActivated(activeEntitiesReadOnly);
-          print("was the stop scene activated: $active, the targets are ${scene.finalTargets}");
-          if(active){
-              return  scene;
-          }
-      }
-      return null;
-    }
 
 
 
-    Scene checkEntitiesForScene(List<Entity> entitiesToCheck) {
-        Scene ret;
-       for(final Entity e in entitiesToCheck) {
-          if(e.isActive) {
-              //yes it includes yourself, what if you're gonna buff your party or something
-              ret = e.performScene(activeEntitiesReadOnly);
-              if(ret != null) {
-                  spotLightEntity = e;
-                  ret.scenario ??=this;
-                  return ret;
-              }
-          }else{
-              ret = e.checkForActivationScenes(activeEntitiesReadOnly);
-              if(ret != null) {
-                  spotLightEntity = e;
-                  ret.scenario ??=this;
-                  return ret;
-              }
-          }
-      }
-       return null;
-    }
+
+
+
 
     Scenario.testScenario(){
         description = "A test scenario with a Pigeon/Bro/Doc ecosystem.";
@@ -232,6 +112,7 @@ class Scenario extends DataObject {
   @override
   void loadFromSerialization(Map<String, dynamic > serialization) {
     // TODO: implement loadFromSerialization
+      //if no seed specified pick a random one
   }
 
   @override
